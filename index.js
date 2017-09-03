213a3ea237e023979dbee6d9804a0251f697c9dd
john vascsinec <pensteeler@gmail.com>
2017-09-03 03:29:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const app = "I don't do much."
+
+function reduce(collection, callback, initialValue) {
+  let result = initialValue;
+
+  collection.forEach((product, index) => {
+    result = callback(result, product, index, collection);
+  });
+
+  return result;
+}
+
+function callback(totalPrice, product) {
+  if (product.discount >= .5) {
+    return totalPrice + product.price;
+  }
+  return totalPrice;
+}
+
+console.log(getTotalAmountForProducts(products, callback)); // prints 25.5
+console.log(couponLocations.reduce(couponCounter, 0)); // also prints 15!
